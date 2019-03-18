394412167c52f97b230ada3bbc4aea3f29fd60d0
Steven Duncan <steven.k.duncan@gmail.com>
2019-03-18 16:17:18 -0600

    tictactoe complete

---
+++ components/ticTacToe/NewGameOrScore.js
@@ -0,0 +1,55 @@
+import react from 'react';
+
+const resetButton = {
+  display: 'inline-block',
+  height: '50%',
+  border: 'none',
+  border: '1px solid black',
+  'alignSelf': 'center',
+  marginLeft: '10px',
+}
+
+const buttonContainer = {
+  display: 'flex',
+}
+
+const NewGameOrScore = (props) => {
+  let moves = props.moves;
+  let gameFinished = props.gameFinished;
+  let currentPlayer = props.currentPlayer;
+  const startNewGame = props.startNewGame;
+  console.log('moves :', moves);
+  console.log('gameFinished :', gameFinished);
+  if (moves === 9 && gameFinished) {
+    return (
+      <div style={buttonContainer}>
+        <span>
+          Game Tied! Play again?
+        </span>
+        <button style={resetButton} onClick={() => startNewGame()}>Reset</button>
+      </div>
+    )
+  }
+  if (moves > 0) {
+    if (!gameFinished) {
+      return (
+        <div className='current-player'>Current Player: {currentPlayer}</div>
+      )
+    } else {
+      return (
+        <div style={buttonContainer}>
+          <div>
+            {`Player ${currentPlayer === 'X' ? 'O' : 'X'} Wins! New Game?`}
+          </div>
+          <button style={resetButton} onClick={() => startNewGame()}>Reset</button>
+        </div>
+      )
+    }
+  } else {
+    return (
+      <div className='current-player'>Current Player: {currentPlayer}</div>
+    )
+  }
+}
+
+export default NewGameOrScore
--- pages/miniApps/ticTacToe.js
+++ pages/miniApps/ticTacToe.js
@@ -2,6 +2,7 @@ import { Component } from 'react';
 import Meta from '../../components/Meta'
 import Navbar from '../../components/Navbar';
 import Footer from '../../components/Footer';
+import NewGameOrScore from '../../components/ticTacToe/NewGameOrScore';
 
 export default class TicTacToe extends Component {
   constructor(props) {
@@ -9,16 +10,19 @@ export default class TicTacToe extends Component {
     this.state = {
       currentPlayer: 'X',
       board: [
-        [[''], [''], ['']],
-        [[''], [''], ['']],
-        [[''], [''], ['']]
+        ['', '', ''],
+        ['', '', ''],
+        ['', '', ''],
       ],
       gameFinished: false,
-      xScore: 0,
-      oScore: 0,
+      score: {
+        X: 0,
+        O: 0,
+      },
       moves: 0,
     }
     this.handleCellClick = this.handleCellClick.bind(this);
+    this.startNewGame = this.startNewGame.bind(this);
   }
 
   handleCellClick(e) {
@@ -30,16 +34,18 @@ export default class TicTacToe extends Component {
       prevMoves++;
       prevBoard[row][column] = this.state.currentPlayer;
       
-      console.log('prevBoard :', prevBoard);
       this.setState({
         board: prevBoard,
         moves: prevMoves,
       }, () => {
         console.log('this.state.moves :', this.state.moves);
         this.handlePlayerToggle();
+        if (this.state.moves >= 5) {
+          console.log('checking game win state from handleCellClick');
+          this.checkForGameFinish();
+        }
       })
     }
-    console.log('e.target.value :', e.target.value);
   }
 
   handlePlayerToggle() {
@@ -55,7 +61,84 @@ export default class TicTacToe extends Component {
   };
   
   checkForGameFinish() {
-    
+    //copy of state board
+    let winFound = false;
+    let winner = '';
+    let prevScore = Object.assign({}, this.state.score);
+    let curBoard = [...this.state.board];
+    console.log('curBoard :', curBoard);
+
+    //make a 90 degree rotated version to check columns
+    let len = curBoard.length - 1;
+    let curBoardRotated = curBoard.map((row, i) => (
+      row.map((cell, j) => curBoard[len - j][i])
+    ))
+    console.log('curBoardRotated :', curBoardRotated);
+
+    //define check rows function
+    const checkRows = m => {
+      m.forEach(row => {
+        if (row[0] !== '' && row.every(v => v === row[0])) {
+          console.log('winner found: ', row[0])
+          winFound = true;
+          winner = row[0]
+        }
+      })
+    }
+
+    //define check diag
+    const checkDiag = m => {
+      let diag = [];
+      for (let i = 0; i < 3; i++) {
+        diag.push(m[i][i]);
+      }
+      console.log('diag :', diag);
+      if (diag[0] !== '' && diag.every(v => v === diag[0])) {
+        winFound = true;
+        winner = diag[0];
+        console.log('winner found :', winner);
+      }
+    }
+
+    //check board and rotated board for wins
+    checkRows(curBoard);
+    checkRows(curBoardRotated);
+    checkDiag(curBoard);
+    checkDiag(curBoardRotated);
+
+    //update state if win is found
+    if (winFound === true) {
+      prevScore[winner]++;
+      console.log('prevScore :', prevScore);
+      this.setState({
+        gameFinished: true,
+        score: prevScore,
+      }, () => {
+        // ! checking score state
+        console.log(this.state.score);
+      })
+    }
+
+    if (this.state.moves === 9) {
+      this.setState({
+        gameFinished: true,
+      })
+    }
+  }
+
+  startNewGame() {
+    console.log('new game requested')
+
+    this.setState({
+      currentPlayer: 'X',
+      board: [
+        ['', '', ''],
+        ['', '', ''],
+        ['', '', ''],
+      ],
+      gameFinished: false,
+      moves: 0
+    })
   }
   
   render() {
@@ -68,17 +151,25 @@ export default class TicTacToe extends Component {
             <div className='title'>Tic Tac Toe</div>
             <div className='players'>
               <div>
-                <div className='player1'>Player X: {this.state.xScore}</div>
-                <div className='player2'>Player O: {this.state.oScore}</div>
+                <div className='player1'>Player X: {this.state.score.X}</div>
+                <div className='player2'>Player O: {this.state.score.O}</div>
               </div>
-              <div className='current-player'>Current Player: {this.state.currentPlayer}</div>
+              <NewGameOrScore
+                gameFinished = {this.state.gameFinished}
+                currentPlayer = {this.state.currentPlayer}
+                startNewGame = {this.startNewGame}
+                moves = {this.state.moves}
+              />
+              
             </div>
             <div className='board-container'>
               <div className='board-cell-0-0 cell'>
                 <button 
                   name='0-0' 
                   value={this.state.board[0][0]}
-                  onClick={(e) => this.handleCellClick(e)} 
+                  onClick={(e) => this.state.gameFinished ? 
+                  null :
+                  this.handleCellClick(e)} 
                   className='cell-button button-0-0'>{this.state.board[0][0]}</button>
 
               </div>
@@ -86,7 +177,9 @@ export default class TicTacToe extends Component {
                 <button 
                   name='0-1' 
                   value={this.state.board[0][1]}
-                  onClick={(e) => this.handleCellClick(e)} 
+                  onClick={(e) => this.state.gameFinished ? 
+                  null :
+                  this.handleCellClick(e)}
                   className='cell-button button-0-1'>{this.state.board[0][1]}</button>
 
               </div>
@@ -95,14 +188,18 @@ export default class TicTacToe extends Component {
                 <button 
                   name='0-2'
                   value={this.state.board[0][2]}
-                  onClick={(e) => this.handleCellClick(e)} 
+                  onClick={(e) => this.state.gameFinished ? 
+                  null :
+                  this.handleCellClick(e)}
                   className='cell-button button-0-2'>{this.state.board[0][2]}</button>
 
               </div>
               <div className='board-cell-1-0 cell'>
                 <button 
                   name='1-0' 
-                  onClick={(e) => this.handleCellClick(e)} 
+                  onClick={(e) => this.state.gameFinished ? 
+                  null :
+                  this.handleCellClick(e)}
                   className='cell-button button-1-0'>{this.state.board[1][0]}</button>
 
               </div>
@@ -110,7 +207,9 @@ export default class TicTacToe extends Component {
                 <button 
                 name='1-1'
                 value={this.state.board[1][1]}
-                onClick={(e) => this.handleCellClick(e)} 
+                onClick={(e) => this.state.gameFinished ? 
+                null :
+                this.handleCellClick(e)}
                 className='cell-button button-1-1'>{this.state.board[1][1]}</button>
 
               </div>
@@ -118,7 +217,9 @@ export default class TicTacToe extends Component {
                 <button 
                 name='1-2'
                 value={this.state.board[1][2]}
-                onClick={(e) => this.handleCellClick(e)} 
+                onClick={(e) => this.state.gameFinished ? 
+                null :
+                this.handleCellClick(e)}
                 className='cell-button button-1-2'>{this.state.board[1][2]}</button>
 
               </div>
@@ -126,7 +227,9 @@ export default class TicTacToe extends Component {
                 <button  
                 name='2-0'
                 value={this.state.board[2][0]}
-                onClick={(e) => this.handleCellClick(e)} 
+                onClick={(e) => this.state.gameFinished ? 
+                null :
+                this.handleCellClick(e)}
                 className='cell-button button-2-0'>{this.state.board[2][0]}</button>
 
               </div>
@@ -134,7 +237,9 @@ export default class TicTacToe extends Component {
                 <button  
                 name='2-1'
                 value={this.state.board[2][1]}
-                onClick={(e) => this.handleCellClick(e)} 
+                onClick={(e) => this.state.gameFinished ? 
+                null :
+                this.handleCellClick(e)}
                 className='cell-button button-2-1'>{this.state.board[2][1]}</button>
 
               </div>
@@ -142,7 +247,9 @@ export default class TicTacToe extends Component {
                 <button 
                 name='2-2'
                 value={this.state.board[2][2]}
-                onClick={(e) => this.handleCellClick(e)} 
+                onClick={(e) => this.state.gameFinished ? 
+                null :
+                this.handleCellClick(e)}
                 className='cell-button button-2-2'>{this.state.board[2][2]}</button>
 
               </div>
@@ -170,8 +277,9 @@ export default class TicTacToe extends Component {
             width: 40vw;
             margin-top: 10px;
             display: flex;
-            flex-direction: row;
+            flex-direction: column;
             justify-content: space-between;
+            align-items: center;
           }
 
           .board-container {
